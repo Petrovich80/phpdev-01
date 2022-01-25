65f558b88306ce1bab3e34e3e95c2d3c915e20ea
Petrovich80 <petrov0803@gmail.com>
2022-01-26 00:16:42 +0900

    sh script2

--- phpdev-init.sh
+++ phpdev-init.sh
@@ -8,6 +8,7 @@ sudo apt install nginx php-fpm --yes;
 sudo apt install htop --yes;
 wget https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/code_1.63.2-1639562499_amd64.deb;
 sudo dpkg -i ./Downloads/code_1.63.2-1639562499_amd64.deb;
+sudo mysql < pass.sql
 tail -n 3 /var/log/nginx/phpmyadmin.access.log;
 
 
